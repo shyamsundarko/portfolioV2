f85447a5a144cec1e1762a1c80cc92f1f1cbb874
shyamsundarko <79359151+shyamsundarko@users.noreply.github.com>
2022-02-06 22:31:39 +0800

    Added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom'
 import blogProject from '../images/blogSpace.png'
 import trialWebsite from '../images/trialWebsite.png'
 import photographyPortfolio from '../images/photographyPortfolio.png'
-
+import denguego from '../images/denguego.png'
+import jokerecommender from '../images/jokerecommender.png'
+import authenticationsystem from '../images/authenticationsystem.png'
 
 import { Card } from 'react-bootstrap'
 import { useEffect, useState } from 'react'
@@ -37,24 +39,46 @@ const Projects = () => {
             <Grid item xs={3}></Grid>
 
             <Grid item xs={12} md={6} data-aos="fade-right">
-            <Link to="/photographyPortfolio" onMouseOver={() => {cardFocusIn()}}>
+                <a href='https://shyamsundarko.github.io/photography_portfolio/' target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
                     <Card className="projectCards" >
                         <Card.Img variant="top" src={photographyPortfolio} className='cardImage'></Card.Img>
                     </Card>
-                </Link>
-                
-                </Grid>
+                </a>    
+            </Grid>
+            <Grid item xs={12} md={6} data-aos="fade-right">
+                <a href="https://github.com/shyamsundarko/DengueGo" target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
+                    <Card className="projectCards">
+                        <Card.Img variant="top" src={denguego} className='cardImage'></Card.Img>
+                    </Card>
+                </a>
+            </Grid>
             <Grid item xs={12} md={6} data-aos="fade-right" >
-                <a href="https://www.mastercard.us/en-us/vision/who-we-are/careers.html" onMouseOver={() => {cardFocusIn()}}>
+                <a href="https://github.com/shyamsundarko/blogSpace" target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
                     <Card className="projectCards">
                         <Card.Img variant="top" src={blogProject} className='cardImage'></Card.Img>
                     </Card>
                 </a>
             </Grid>
+            
             <Grid item xs={12} md={6} data-aos="fade-right">
-                <a href="https://www.mastercard.us/en-us/vision/who-we-are/careers.html" onMouseOver={() => {cardFocusIn()}}>
+                <a href="https://shyamsundarko.github.io/personalWebsite/" target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
                     <Card className="projectCards">
                         <Card.Img variant="top" src={trialWebsite} className='cardImage'></Card.Img>
+                    </Card>
+                </a>
+            </Grid>
+            
+            <Grid item xs={12} md={6} data-aos="fade-right">
+                <a href="https://github.com/shyamsundarko/Joke-Recommender-System" target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
+                    <Card className="projectCards">
+                        <Card.Img variant="top" src={jokerecommender} className='cardImage'></Card.Img>
+                    </Card>
+                </a>
+            </Grid>
+            <Grid item xs={12} md={6} data-aos="fade-right">
+                <a href="https://github.com/shyamsundarko/AuthenticationSystem" target='_blank' rel='noreferrer' onMouseOver={() => {cardFocusIn()}}>
+                    <Card className="projectCards">
+                        <Card.Img variant="top" src={authenticationsystem} className='cardImage'></Card.Img>
                     </Card>
                 </a>
             </Grid>
