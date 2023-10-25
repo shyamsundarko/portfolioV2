aa9dcd783ade90e6d98849711379f1737a84e473
Palaniselvam Shyam Sundar <palaniselvam.s@northeastern.edu>
2023-10-25 10:51:17 -0400

    fixed some styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,39 +18,39 @@ const Skills = () => {
   return (
       <Grid container id="skillsContainer">
             
-            <Grid item xs={12} md={4}>
+            <Grid item xs={12} lg={4}>
                 <h2 id="skillsTitle" className="titles">Skills & {window.innerWidth<550 ? <></> : <br/> }Competencies</h2>
             </Grid>
-            <Grid item xs={12} md={1} />
-            <Grid container item xs={12} md={7}>
-                <Grid item xs={12} md={6}>
+            <Grid item xs={12} lg={1} />
+            <Grid container item xs={12} lg={7}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={react} value="80" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={java} value="75" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={html5} value="90" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={css3} value="85" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={git} value="80" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={figma} value="90" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={node} value="70" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={mysql} value="70" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={XD} value="90" />
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} lg={6}>
                     <Skill skillImg={PS} value="76" />
                 </Grid>
             </Grid>
