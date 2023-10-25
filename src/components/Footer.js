a58751ae59656ee0fe2c2ffda755bdd243c87ed3
Palaniselvam Shyam Sundar <palaniselvam.s@northeastern.edu>
2023-10-25 11:34:03 -0400

    mail change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,14 +13,13 @@ const Footer = () => {
                 <Grid item xs={2} ></Grid>
                 <Grid item xs={2} ><a href="https://www.linkedin.com/in/shyam-s-208166137/" target="_blank"><img src={linkedin} alt="Linkedin logo" className="socialLogo"/></a></Grid>
                 <Grid item xs={2} ><a href="https://github.com/shyamsundarko" target="_blank"><img src={github} alt="Github logo" className="socialLogo"/></a></Grid>
-                <Grid item xs={2} ><a href="mailto:SHYAMSUN001@e.ntu.edu.sg" target="_blank"><img src={outlook} alt="Outlook logo" className="socialLogo"/></a></Grid>
+                <Grid item xs={2} ><a href="mailto:palaniselvam.s@northeastern.edu" target="_blank"><img src={outlook} alt="Outlook logo" className="socialLogo"/></a></Grid>
                 <Grid item xs={2} ><a href="https://www.instagram.com/byamiloboy/?hl=en" target="_blank"><img src={instagram} alt="Instagram logo" className="socialLogo"/></a></Grid>
                 <Grid item xs={2} ></Grid>
             </Grid>
             
             <Grid item xs={2} md={4}></Grid>
             <Grid item xs={12}><a href="mailto:shyamsundarko@gmail.com" target="_blank" id="email">shyamsundarko@gmail.com</a></Grid>
-            {/* <Grid item xs={12} id="footerText">Say hello!</Grid> */}
             <Grid item xs={12} id="footerText">Say hello!</Grid>
         </Grid>
     )
